9156d0992513c7ddcc4027b6c43f242da5894c91
James Brown <bjames1@vt.edu>
2020-01-14 17:55:35 -0500

    Add files via upload

--- gsheetData.py
+++ gsheetData.py
@@ -16,7 +16,6 @@ client = gspread.authorize(creds);
 
 sheet = client.open("RVHIdatabase_variableListing")
 
-
 # worksheets[0]
 # worksheets[1]
 # worksheets[2]
@@ -43,25 +42,64 @@ N30DAYsubhistUM_TABLE = N30DAYsubhistUM_WS.get_all_values();
 
 
 ##Convert table data into a dataframe
-DemographicInfo = pd.DataFrame(DemographicInfo_TABLE[1:], columns=DemographicInfo_TABLE[0]);
-SubstancesList = pd.DataFrame(SubstancesList_TABLE[1:], columns=SubstancesList_TABLE[0]);
-N30DAYsubhistYN = pd.DataFrame(N30DAYsubhistYN_TABLE[1:], columns=N30DAYsubhistYN_TABLE[0]);
-N30DAYsubhistAPD = pd.DataFrame(N30DAYsubhistAPD_TABLE[1:], columns=N30DAYsubhistAPD_TABLE[0]);
-N30DAYsubhistTPD = pd.DataFrame(N30DAYsubhistTPD_TABLE[1:], columns=N30DAYsubhistTPD_TABLE[0]);
-N30DAYsubhistDPM = pd.DataFrame(N30DAYsubhistDPM_TABLE[1:], columns=N30DAYsubhistDPM_TABLE[0]);
-N30DAYsubhistUM = pd.DataFrame(N30DAYsubhistUM_TABLE[1:], columns=N30DAYsubhistUM_TABLE[0]);
+DemographicInfo_DF = pd.DataFrame(DemographicInfo_TABLE[1:], columns=DemographicInfo_TABLE[0]);
+SubstancesList_DF = pd.DataFrame(SubstancesList_TABLE[1:], columns=SubstancesList_TABLE[0]);
+N30DAYsubhistYN_DF = pd.DataFrame(N30DAYsubhistYN_TABLE[1:], columns=N30DAYsubhistYN_TABLE[0]);
+N30DAYsubhistAPD_DF = pd.DataFrame(N30DAYsubhistAPD_TABLE[1:], columns=N30DAYsubhistAPD_TABLE[0]);
+N30DAYsubhistTPD_DF = pd.DataFrame(N30DAYsubhistTPD_TABLE[1:], columns=N30DAYsubhistTPD_TABLE[0]);
+N30DAYsubhistDPM_DF = pd.DataFrame(N30DAYsubhistDPM_TABLE[1:], columns=N30DAYsubhistDPM_TABLE[0]);
+N30DAYsubhistUM_DF = pd.DataFrame(N30DAYsubhistUM_TABLE[1:], columns=N30DAYsubhistUM_TABLE[0]);
+
+##Convert dataframe to dictionary
+DemographicInfo_DICT = DemographicInfo_DF.to_dict()
+SubstancesList_DICT = SubstancesList_DF.to_dict()
+N30DAYsubhistYN_DICT = N30DAYsubhistYN_DF.to_dict()
+N30DAYsubhistAPD_DICT = N30DAYsubhistAPD_DF.to_dict()
+N30DAYsubhistTPD_DICT = N30DAYsubhistTPD_DF.to_dict()
+N30DAYsubhistDPM_DICT = N30DAYsubhistDPM_DF.to_dict()
+N30DAYsubhistUM_DICT = N30DAYsubhistUM_DF.to_dict()
 
 
-# DemographicInfo
-#
-# SubstancesList
-#
-# N30DAYsubhistYN
-#
-# N30DAYsubhistAPD
-#
-# N30DAYsubhistTPD
-#
-# N30DAYsubhistDPM
-#
-# N30DAYsubhistUM
+def substance_index(key, case):
+
+    if case == 'u':
+
+        if isinstance(key, str):
+            substance_upperCase = list(SubstancesList_DICT['substance_upperCase'].values())
+            index = substance_upperCase.index(key);
+
+        if isinstance(key, int):
+            substance_upperCase = list(SubstancesList_DICT['substance_upperCase'].values())
+            index = substance_upperCase[key];
+
+    if case == 'l':
+
+        if isinstance(key, str):
+            substance_lowerCase = list(SubstancesList_DICT['substance_lowerCase'].values())
+            index = substance_lowerCase.index(key);
+
+        if isinstance(key, int):
+            substance_lowerCase = list(SubstancesList_DICT['substance_lowerCase'].values())
+            index = substance_lowerCase[key];
+
+
+    if case == 't':
+
+        if isinstance(key, str):
+            substance_pageTitle = list(SubstancesList_DICT['substance_pageTitle'].values())
+            index = substance_pageTitle.index(key);
+
+        if isinstance(key, int):
+            substance_pageTitle = list(SubstancesList_DICT['substance_pageTitle'].values())
+            index = substance_pageTitle[key];
+
+    return index
+
+
+
+
+cases = ['u', 'l', 't'];
+nSubstances = len(SubstancesList_DF);
+substance_list = list(SubstancesList_DF['substance_upperCase']);
+
+gsheetVars = list(globals());
